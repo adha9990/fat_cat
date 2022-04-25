b9d16178db2d13a9c4daa02dc0d56250c1866f93
Bluestar <adha9990@gmail.com>
2022-04-25 21:00:47 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -20,9 +20,9 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:adha9990/fat_cat.git master:gh-pages
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 # 除此之外，也可以改走 HTTPS 模式
-# git push -f https://github.com/hsiangfeng/HexfootMusic.git master:gh-pages
+git push -f https://github.com/adha9990/fat_cat.git master:gh-pages
 
 cd -
